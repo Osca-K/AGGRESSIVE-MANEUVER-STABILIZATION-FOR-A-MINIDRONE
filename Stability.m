987cb756b62949e951e103a73f9df3a3b522cf82
Osca Kholopha <118875945+Osca-K@users.noreply.github.com>
2025-05-18 00:17:17 +0200

    Stability analysis
    using pzmap to plot zero-pole for each transfer function for stability analysis

---
+++ Stability.m
@@ -0,0 +1,55 @@
+clear
+clc
+close all
+
+% Script for ploting the pole-zero map of four transfer functions
+m=0.506;
+Ixx=8.12e-5;
+Iyy=8.12e-5;
+Izz=6.12e-5;
+
+% Transfer Function for elevation, roll, pitch, and yaw 
+TFe = tf(1, [m 0 0]);
+TFr = tf(1, [Ixx 0 0]);
+TFp = tf(1, [Iyy 0 0]);
+TFy = tf(1, [Izz 0 0]);
+
+
+set(groot, 'DefaultAxesFontName', 'Times New Roman');
+set(groot, 'DefaultAxesFontSize', 14);
+set(groot, 'DefaultTextFontName', 'Times New Roman');
+set(groot, 'DefaultTextFontSize', 14);
+
+
+figure;
+pzmap(TFe);
+grid on;
+title('Pole-Zero for Elevation,', 'FontWeight', 'normal');
+xlim([-0.5 0.5]);
+ylim([-0.5 0.5]);
+
+
+
+figure;
+pzmap(TFr);
+grid on;
+title('Pole-Zero for Roll', 'FontWeight', 'normal');
+xlim([-0.5 0.5]);
+ylim([-0.5 0.5]);
+
+
+figure;
+pzmap(TFp);
+grid on;
+title('Pole-Zero for Pitch', 'FontWeight', 'normal');
+xlim([-0.5 0.5]);
+ylim([-0.5 0.5]);
+
+
+figure;
+pzmap(TFy);
+grid on;
+title('Pole-Zero for Yaw', 'FontWeight', 'normal');
+xlim([-0.5 0.5]);
+ylim([-0.5 0.5]);
+
